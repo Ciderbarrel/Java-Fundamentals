5ddc0e782ed9a338d86000172b1b7ac2ac026da8
ciderbarrel <mooremichael77@gmail.com>
2023-11-20 14:04:18 -0500

    Datatype Labs Exercise 1

--- src/datatypes_operators/labs/Exercise_01.java
+++ src/datatypes_operators/labs/Exercise_01.java
@@ -2,14 +2,12 @@ package datatypes_operators.labs;
 
 /**
  * Data Types and Operators Exercise 1: Variable declaration and Initialization
- *
- *      Write the necessary code to complete the following:
- *
- *      1. Declare an int, a double, a float and a char
- *      2. Assign appropriate values to each
- *      3. Print out each variable to the console
- *
- *
+ * <p>
+ * Write the necessary code to complete the following:
+ * <p>
+ * 1. Declare an int, a double, a float and a char
+ * 2. Assign appropriate values to each
+ * 3. Print out each variable to the console
  */
 
 public class Exercise_01 {
@@ -17,6 +15,11 @@ public class Exercise_01 {
     public static void main(String[] args) {
 
         // write code here
+        int xInt = 1;
+        double xDouble = 2.2;
+        float xFloat = 3.3f;
+        char xChar = 'A';
 
+        System.out.println("int = " + xInt + ", double = " + xDouble + ", float = " + xFloat + ", char = " + xChar);
     }
 }

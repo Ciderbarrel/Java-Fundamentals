fd28344f040ed0e62db33dc0610242e65eddf1d7
ciderbarrel <mooremichael77@gmail.com>
2023-11-20 17:46:29 -0500

    Datatype Labs Exercise 6

--- src/datatypes_operators/labs/Exercise_06.java
+++ src/datatypes_operators/labs/Exercise_06.java
@@ -2,23 +2,40 @@ package datatypes_operators.labs;
 
 /**
  * Data Types and Operators Exercise 6: 2D Array
- *
- *      Creat and populate a 5x5 2D array with multiples of 3, starting with 3. Once populated, print out the results.
- *
- *      The output should look something like this:
- *
- *      3 6 9 12 15
- *      18 21 24 27 30
- *      ...
- *      ...
- *      ...
- *
+ * <p>
+ * Create and populate a 5x5 2D array with multiples of 3, starting with 3. Once populated, print out the results.
+ * <p>
+ * The output should look something like this:
+ * <p>
+ * 3 6 9 12 15
+ * 18 21 24 27 30
+ * ...
+ * ...
+ * ...
  */
 
 public class Exercise_06 {
 
     public static void main(String[] args) {
 
-        int[][] twoDArray = new int[5][5];
+        final int x = 5;
+        final int y = 5;
+        int[][] twoDArray = new int[x][y];
+
+        int sum = 0;
+
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                sum += 3;
+                twoDArray[i][j] = sum;
+            }
+        }
+
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j < y; j++) {
+                System.out.print(twoDArray[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }

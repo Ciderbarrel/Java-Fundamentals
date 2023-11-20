5c22e1537680e0ecc31f71e8433420703c1015b2
ciderbarrel <mooremichael77@gmail.com>
2023-11-20 14:10:38 -0500

    Datatype Labs Exercise 2

--- src/datatypes_operators/labs/Exercise_02.java
+++ src/datatypes_operators/labs/Exercise_02.java
@@ -2,21 +2,20 @@ package datatypes_operators.labs;
 
 /**
  * Data Types and Operators Exercise 2: Type conversion and casting
- *
- *      Write the necessary code to demonstrate type conversion:
- *
- *      Widening:
- *      1. Declare and initialize an int with the value 10
- *      2. Declare a double and set it equal to the int
- *      3. Print out the double
- *
- *      Narrowing (with casting):
- *      1. Declare and initialize a new double with the value 10.5
- *      2. Declare a new int and set it equal to the double (make sure to cast the double to an int)
- *      3. Print out the int
- *
- *      Notice how each type is converted. In which direction is information lost?
- *
+ * <p>
+ * Write the necessary code to demonstrate type conversion:
+ * <p>
+ * Widening:
+ * 1. Declare and initialize an int with the value 10
+ * 2. Declare a double and set it equal to the int
+ * 3. Print out the double
+ * <p>
+ * Narrowing (with casting):
+ * 1. Declare and initialize a new double with the value 10.5
+ * 2. Declare a new int and set it equal to the double (make sure to cast the double to an int)
+ * 3. Print out the int
+ * <p>
+ * Notice how each type is converted. In which direction is information lost?
  */
 
 public class Exercise_02 {
@@ -24,6 +23,13 @@ public class Exercise_02 {
     public static void main(String[] args) {
 
         // write code here
+        int xIntWide = 10;
+        double yDoubleWide = xIntWide;
+        System.out.println("Value = " + yDoubleWide);
+
+        double yDoubleNarrow = 10.5;
+        int xIntNarrow = (int) yDoubleNarrow;
+        System.out.println("Value = " + xIntNarrow);
 
     }
 }

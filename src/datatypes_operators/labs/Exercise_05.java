c2a3d0e29e5f99975259c5ba2f225b03bfaba666
ciderbarrel <mooremichael77@gmail.com>
2023-11-20 17:34:27 -0500

    Datatype Labs Exercise 5

--- src/datatypes_operators/labs/Exercise_05.java
+++ src/datatypes_operators/labs/Exercise_05.java
@@ -1,21 +1,58 @@
 package datatypes_operators.labs;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  * Data Types and Operators Exercise 5: More arrays
- *
- *      Using the array below, take in a number from 1-10 from the user and print out the index of that
- *      element.
- *
+ * <p>
+ * Using the array below, take in a number from 1-10 from the user and print out the index of that
+ * element.
  */
 
-public class Exercise_05{
+public class Exercise_05 {
+
+    private static int findInt(int[] array, int searchValue) {
+        boolean continueLoop = true;
+
+        int returnValue = -1;
+        int index = 0;
+
+        while (continueLoop) {
+            if (index < array.length) {
+                if (searchValue == array[index]) {
+                    returnValue = index;
+                    continueLoop = false;
+                } else {
+                    index++;
+                }
+            } else {
+                continueLoop = false;
+            }
+        }
+
+        return returnValue;
+    }
 
     public static void main(String[] args) {
 
         int[] array = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
 
-        // write code here
+        System.out.print("Enter a number 1-10 to get its index: ");
 
+        // create scanner
+        Scanner scanner = new Scanner(System.in);
+        try {
+            int guess = scanner.nextInt();
+            int index = findInt(array, guess);
 
+            if (index < array.length && index >= 0)
+                System.out.println(guess + " is at index " + index);
+            else {
+                System.out.println("The number is not in the array");
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid value");
+        }
     }
 }

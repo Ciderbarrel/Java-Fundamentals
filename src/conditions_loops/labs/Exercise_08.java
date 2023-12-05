3561cf80d98971ed8a22cbd1406d28efc6a69246
ciderbarrel <mooremichael77@gmail.com>
2023-12-05 16:59:54 -0500

    Condition Labs Exercise 8

--- src/conditions_loops/labs/Exercise_08.java
+++ src/conditions_loops/labs/Exercise_08.java
@@ -2,10 +2,33 @@ package conditions_loops.labs;
 
 /**
  * Conditions and Loops Exercise 8: do-while loop
- *
- *      Use a "do-while" loop print out every third number counting backwards from 1000 to 1.
- *
+ * <p>
+ * Use a "do-while" loop print out every third number counting backwards from 1000 to 1.
  */
 
 public class Exercise_08 {
+
+    private static final int MAX_PER_LINE = 19;
+
+    public static void main(String[] args) {
+        int i = 1000;
+        int counter = 0;
+        int numToPrintOnLine = MAX_PER_LINE;
+
+        do {
+            if (counter == 0) {
+                System.out.print(i + " ");
+                if (numToPrintOnLine == 0) {
+                    System.out.println();
+                    numToPrintOnLine = MAX_PER_LINE;
+                } else {
+                    numToPrintOnLine--;
+                }
+                counter = 2;
+            } else {
+                counter--;
+            }
+            i--;
+        } while (i > 0);
+    }
 }

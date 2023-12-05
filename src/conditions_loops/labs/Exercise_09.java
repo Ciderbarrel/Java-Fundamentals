fd8fd3011d8a146d392d3570682199e924634e96
ciderbarrel <mooremichael77@gmail.com>
2023-12-05 17:03:26 -0500

    Condition Labs Exercise 9

--- src/conditions_loops/labs/Exercise_09.java
+++ src/conditions_loops/labs/Exercise_09.java
@@ -2,10 +2,19 @@ package conditions_loops.labs;
 
 /**
  * Conditions and Loops Exercise 9: break
- *
- *      Demonstrate the use of the "break" statement to exit a loop.
- *
+ * <p>
+ * Demonstrate the use of the "break" statement to exit a loop.
  */
 
 public class Exercise_09 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            if (i > 5) {
+                System.out.println(i + " > 5, BREAK");
+                break;
+            } else {
+                System.out.println("Print until i > 5: i == " + i);
+            }
+        }
+    }
 }
